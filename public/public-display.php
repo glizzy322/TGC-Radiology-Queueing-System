eb8199ed17e7f4c9e81afcb69ff79a7444845852
Glener Guibone <g.guibone.142629.tc@umindanao.edu.ph>
2026-08-06 12:03:10 +0800

    chore: remove obsolete prototype files

--- public/public-display.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require dirname(__DIR__) . '/app/Controllers/PublicDisplayController.php';
-
-(new \App\Controllers\PublicDisplayController())->show();
--- public/receptionist.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require dirname(__DIR__) . '/app/Controllers/ReceptionController.php';
-
-(new \App\Controllers\ReceptionController())->show();
